7d0d236d23751cd1908ab2a5baed9b147b898661
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-07-10 19:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //your JS code here. If required.
 function ArrayProcess(){
-	const initialPromise = new promise((resolve)=>{
-		setTimeOut(()=>{
-	const ArrayNumbers = [1,2,3,4];
-			resolve(AarrayNumbers);// pass this array to the next .then()
+	const initialPromise = new Promise((resolve)=>{
+	setTimeOut(()=>{
+	const ArrayNumber = [1,2,3,4];
+			resolve(AarrayNumber);// pass this array to the next .then()
 		},3000);
 	});
 
 	//now i am going to start chaining promise process yaha se
-	initialPromise.then((AarrayNumbers)=>{        // First .then() receives the numbers array
+	initialPromise.then((AarrayNumber)=>{        // First .then() receives the numbers array
 // Create another promise for filtering even numbers
-		return new promise((resolve)=>{
+		return new Promise((resolve)=>{
 // Filter out odd numbers; keep even ones only
 	const even = ArrayNumber.filter((num)=>{//num is the current number in the array being checked.
 // num is not declared manually like let num = ....
 // It is declared implicitly as a parameter of the function passed into .filter().		
 		return num % 2 === 0;
 				});
-			setTimeOut(()=>{
+			setTimeout(()=>{
 				// Display the even numbers (e.g., 2, 4) in the output div
 				document.getElementById("output").textContent = even.join(",");
 				// Pass the even numbers to the next .then()
@@ -27,17 +27,21 @@ function ArrayProcess(){
 		
 	}).then((even)=>{
 		// Create another promise for multiplying by 2
-		return new promise((resolve)=>{
+		return new Promise((resolve)=>{
 		const doubleNumber = even.map((num)=>{
 			return num*2;
 		});
-			setTimeOut(()=>{
+			setTimeout(()=>{
 				document.getElementById("output").textContent = doubleNumber.join(",");
+				 // Resolve the final result (not needed further, but good practice)
+              resolve(doubledNumber);
 			},2000);
+			
 		});
 	});
 	
 }
+
 
 
 
